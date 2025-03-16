72ef2bfc3ea85de6eb935ad152ac116400e91a1f
Daniel Santos <danieldiefsantos@gmail.com>
2025-03-16 15:30:12 -0300

    feat: criando app com middlewares, swagger e rotas

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
+import dotenv from 'dotenv';
+import { validateToken } from './middlewares/validateToken';
+import loginRouter from './routes/login';
+import userRouter from './routes/user';
+import wavesRouter from './routes/wave';
+
+dotenv.config();
+
+const app: Express = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  validateToken({ req, res, next });
+})
+
+// Swagger configuration
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API REST com Express e TypeScript',
+      version: '1.0.0',
+      description: 'Uma API REST construída com Express e TypeScript'
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.API_PORT}`,
+        description: 'Servidor de desenvolvimento'
+      }
+    ],
+  },
+  apis: ['./src/routes/*.ts', './src/models/*.ts']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Routes
+app.use('/api', loginRouter);
+
+app.use('/api/user', userRouter);
+
+app.use('/api/waves', wavesRouter);
+
+export default app;
